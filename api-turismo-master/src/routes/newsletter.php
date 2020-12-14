63283111fb414c8cd9578f9bb8d7710dd5127d8f
Agustin <tincho082009@gmail.com>
2020-12-14 11:05:32 -0300

    Update newsletter
    Agregue el update y tambien agregue un get adicional

--- api-turismo-master/src/routes/newsletter.php
+++ api-turismo-master/src/routes/newsletter.php
@@ -7,7 +7,17 @@
 
      //[GET]
     //Todas los Newsletters
-    $app->get("/newsletters", function (Request $request, Response $response, array $args) {
+    $app->get("/newsletter/all", function (Request $request, Response $response, array $args) {
+        $xSQL = "SELECT * FROM newsletter";
+        $respuesta = dbGet($xSQL);
+        return $response
+            ->withStatus(200)
+            ->withHeader("Content-Type", "application/json")
+            ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    });
+
+    //Todo los Newsletters activos
+    $app->get("/newsletter/activos", function (Request $request, Response $response, array $args) {
         $xSQL = "SELECT * FROM newsletter WHERE activo = 1";
         $respuesta = dbGet($xSQL);
         return $response
@@ -16,6 +26,44 @@
             ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
     });
 
+    //Actualizar Newsletter
+    $app->patch("/newsletter/{id:[0-9]+}", function (Request $request, Response $response, array $args) {
+        $reglas = array(
+            "email" => array(
+                "min" => 5,
+                "max" => 100,
+                "tag" => "Email"
+            ),
+            "activo" => array(
+                "tag" => "Activo"
+            )
+        );
+                
+        $parsedBody = $request->getParsedBody();
+        $validar = new Validate();
+        if($validar->validar($parsedBody, $reglas)) {
+            $data = array(
+                "email" => $parsedBody["email"],
+                "activo" => $parsedBody["activo"]
+            );
+            $respuesta = dbPatchWithData("newsletter", $args["id"], $data);       
+            return $response
+                ->withStatus(200) //Ok
+                ->withHeader("Content-Type", "application/json")
+                ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        } else {
+            $resperr = new stdClass();
+            $resperr->err = true;
+            $resperr->errMsg = "Hay errores en los datos suministrados";
+            $resperr->errMsgs = $validar->errors();
+            return $response
+                ->withStatus(409) //Conflicto
+                ->withHeader("Content-Type", "application/json")
+                ->write(json_encode($resperr, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        }
+    });
+
+    
     //Crear Nuevo Newsletter
     $app->post("/newsletter", function (Request $request, Response $response, array $args) {
         $reglas = array(
@@ -33,7 +81,8 @@
                 "email" => $parsedBody["email"],
                 "activo" => 1
             );
-            $respuesta = dbPostWithData("newsletter", $data);
+            $respuesta = dbPostWithData("newsletter", $data);    
+            
             return $response
                 ->withStatus(200) //Ok
                 ->withHeader("Content-Type", "application/json")
