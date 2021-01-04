d2dbbc77aa254a40dad50cff3031515fac2da9f1
Agustin <tincho082009@gmail.com>
2021-01-04 10:45:07 -0300

    Get Agencias Adheridos a  Dosep

--- api-turismo-master/src/routes/agencias.php
+++ api-turismo-master/src/routes/agencias.php
@@ -27,6 +27,17 @@ $app->get("/agencias/adhiereCovid", function (Request $request, Response $respon
         ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 });
 
+$app->get("/agencias/adhiereDosep", function (Request $request, Response $response, array $args) {
+    $xSQL = "SELECT agencias_viaje.*, ciudades.nombre AS ciudad FROM agencias_viaje";
+    $xSQL .= " INNER JOIN ciudades ON agencias_viaje.idlocalidad = ciudades.id";
+    $xSQL .= " WHERE agencias_viaje.adhiereDosep > 0";
+    $xSQL .= " ORDER BY agencias_viaje.idlocalidad";
+    $respuesta = dbGet($xSQL);
+    return $response
+        ->withStatus(200) 
+        ->withHeader("Content-Type", "application/json")
+        ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+});
 //Guías de Turismo
 
 $app->post("/addagenciadeviajes", function (Request $request, Response $response, array $args) {
