68d9e2960d818a129a0f972d91b8fa973d51fe63
BrenGu <41438249+BrenGu@users.noreply.github.com>
2022-10-05 13:53:53 -0300

    update cajeros

--- api-turismo-master/src/routes/cajeros.php
+++ api-turismo-master/src/routes/cajeros.php
@@ -14,5 +14,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 });
 
 
+    //Todas las Oficinas de una Localidad
+    $app->get("/cajeros/localidad/{id:[0-9]+}", function (Request $request, Response $response, array $args) {
+        $xSQL = " SELECT bancos.nombre as nombre, cajeros.*  FROM cajeros, bancos ";
+        $xSQL .= " WHERE cajeros.tpo_bco = bancos.id";
+        $xSQL .= " AND  cajeros.idlocalidad = ". $args["id"];
+        $respuesta = dbGet($xSQL);
+        return $response
+            ->withStatus(200)
+            ->withHeader("Content-Type", "application/json")
+            ->write(json_encode($respuesta, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    });
 
 ?>
